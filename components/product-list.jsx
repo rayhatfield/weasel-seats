0a46901f14d32f18b05a4a591ef256dc904fa24c
Ray Hatfield <ray.hatfield@gmail.com>
2020-10-26 01:17:24 -0500

    paint; distinguish brand from product name

--- components/product-list.jsx
+++ components/product-list.jsx
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types';
 
 import Rating from './rating';
 
+import styles from './product-list.module.css';
+
 export default function ProductList ({ products }) {
     return (
-        <ul>
+        <ul className={styles.productList}>
             { products.map((product, i) => (
               <li key={i + product.brand + product.name}>
                 <Product product={product} />
@@ -16,7 +18,7 @@ export default function ProductList ({ products }) {
 }
 
 const Product = ({product: {brand, name, rating}}) => (
-    <div>
-        <div>{brand}: {name} <Rating rating={rating} /></div>
-    </div>
+  <div>
+    <span className={styles.brand}>{brand}</span> <span className={styles.productName}>{name}</span> <Rating rating={rating} />
+  </div>
 );
